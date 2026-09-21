32f809a13badf5614d921dc07200ea9bdf89d535
EniolaJonathanDare-S <G00476368@atu.ie>
2026-09-21 13:48:53 +0100

    Lab 1: create and use Book objects

--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -5,13 +5,27 @@ package ie.atu.oop.week1;
 public class Main {
     public static void main(String[] args) {
 
-        Book firstBook = new Book();
-        firstBook.title = "Dune";
-        firstBook.author = "Frank Herbert";
-        firstBook.pageCount = 412;
+        Book firstBook = createBook("Dune", "Frank Herbert", 412);
+        Book secondBook = createBook("Clean Code", "Robert C. Martin", 464);
+        Book thirdBook = createBook("The C programming Language", "Kernighan and Ritchie", 274);
 
         firstBook.displayDetails();
+        System.out.println("\n");
+        secondBook.displayDetails();
+        System.out.println("\n");
+        thirdBook.displayDetails();
+        System.out.println("\n");
+
+
         firstBook.borrowBook();
         firstBook.displayDetails();
+    }
+
+    private static Book createBook(String title, String author, int pageCount){
+        Book book = new Book();
+        book.title = title;
+        book.author = author;
+        book.pageCount = pageCount;
+        return book;
     }
 }
